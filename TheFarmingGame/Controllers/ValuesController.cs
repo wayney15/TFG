f6ca46fa338553a0a95ae50b44f2c2caac838c56
TT22000 <tlee44@gmu.edu>
2023-04-30 16:35:56 -0400

    Sample API Call

---
+++ TheFarmingGame/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheFarmingGame.Domains;
+
+namespace TheFarmingGame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersAPIController : Controller
+    {
+        // GET: api/GetAllUsers 
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            List<User> students = new List<User>
+           {
+           new User{
+                              Id = 1234,
+                              UserName = "Thomas",
+                              Password = "123456",
+                              Alias = "TT",
+                              Money = 12.34M
+                              },
+           new User{
+                              Id = 1244,
+                              UserName = "Jack",
+                              Password = "12345",
+                              Alias = "JJ",
+                              Money = 12.44M
+                              },
+           };
+            return students;
+        }
+    }
+}
