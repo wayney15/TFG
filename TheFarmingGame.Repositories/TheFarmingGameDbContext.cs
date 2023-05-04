517d6b7497692e47ba17c597957ddba284f7172d
Wayne Yuan <44449663+wayney15@users.noreply.github.com>
2023-05-04 01:50:13 -0400

    adding data access

--- TheFarmingGame.Repositories/TheFarmingGameDbContext.cs
+++ TheFarmingGame.Repositories/TheFarmingGameDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Xml;
 using TheFarmingGame.Domains;
 
 namespace TheFarmingGame.Repositories
@@ -10,5 +11,13 @@ namespace TheFarmingGame.Repositories
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Land> Land  { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(e => e.Alias)
+                .IsUnique();
+        }
     }
 }
