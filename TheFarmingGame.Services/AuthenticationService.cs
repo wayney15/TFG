7c5314975597b56a2038e26de90e9985847b59ec
TT22000 <tlee44@gmu.edu>
2023-05-04 21:26:17 -0400

    Add depencies for hashing

--- TheFarmingGame.Services/AuthenticationService.cs
+++ TheFarmingGame.Services/AuthenticationService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
+using Konscious.Security.Cryptography;
 using System.Threading.Tasks;
 using TheFarmingGame.Domains;
 
