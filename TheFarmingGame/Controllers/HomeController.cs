582082895f2ee5a0226a7484cdcacfec0e9ef637
TT22000 <tlee44@gmu.edu>
2023-05-04 21:22:26 -0400

    Remove Login/Register routes

--- TheFarmingGame/Controllers/HomeController.cs
+++ TheFarmingGame/Controllers/HomeController.cs
@@ -5,24 +5,10 @@ namespace TheFarmingGame.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("[controller]")]
+        [Route("[controller]")]
         public ContentResult Home()
         {
             var html = System.IO.File.ReadAllText("Views/Home.cshtml");
-            return base.Content(html, "text/html");
-        }
-
-        [HttpGet("Login")]
-        public ContentResult Login()
-        {
-            var html = System.IO.File.ReadAllText("Views/Login.cshtml");
-            return base.Content(html, "text/html");
-        }
-
-        [HttpGet("Register")]
-        public ContentResult Register()
-        {
-            var html = System.IO.File.ReadAllText("Views/Register.cshtml");
             return base.Content(html, "text/html");
         }
     }
