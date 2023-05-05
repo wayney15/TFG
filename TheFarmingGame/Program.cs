e39d84a0297a25de1216015570c98f6817abea62
TT22000 <tlee44@gmu.edu>
2023-05-04 21:27:52 -0400

    Remove edit

--- TheFarmingGame/Program.cs
+++ TheFarmingGame/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<TheFarmingGameDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
