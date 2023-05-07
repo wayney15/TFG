309348068e2dda133306049fb9cbe78d116df2ae
Wayne Yuan <44449663+wayney15@users.noreply.github.com>
2023-05-07 12:33:23 -0400

    adding dependency injection on services

--- TheFarmingGame.Services/BidService.cs
+++ TheFarmingGame.Services/BidService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheFarmingGame.Repositories;
 
 namespace TheFarmingGame.Services
 {
     public class BidService : IBidService
     {
+        private readonly IBidRepository _bidRepository;
+        public BidService(IBidRepository bidRepository)
+        {
+            _bidRepository = bidRepository;
+        }
     }
 }
