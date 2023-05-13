876957c3997a4d7c697bbfd7b8eb77e5feffc392
Wayne Yuan <44449663+wayney15@users.noreply.github.com>
2023-05-13 18:15:26 -0400

    add change land alias

---
+++ TheFarmingGame.Domains/Requests/RenameRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFarmingGame.Domains.Requests
+{
+    public class RenameRequest
+    {
+        [Required]
+        [RegularExpression(@"^[A-Za-z][A-Za-z0-9_!+=?]{6,20}$", ErrorMessage = "Land alias should be 6-20 characters long with alphnumerics and _!+=?")]
+        public string Alias { get; set; }
+        public int LandId { get; set; }
+    }
+}
